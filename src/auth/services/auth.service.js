c02df4a9f54c2a9f019b8c45d99f595b5088ca81
andres2906@gmail.com <andres2906@gmail.com>
2020-03-07 12:03:54 -0500

    Add functionality to register and login user in AuthService.

--- src/auth/services/auth.service.js
+++ src/auth/services/auth.service.js
@@ -5,10 +5,43 @@ const AuthService = {
       this._instance = {
         users: [],
         currentUser: null,
-        registerUser(user) {
-          this.users.push(user);
+        registerUser(_username, _password) {
+          const index = this.users.findIndex(
+            user => user.username === _username,
+          );
+
+          if (index < 0) {
+            const currentUser = {username: _username, password: _password};
+            this.users.push(currentUser);
+            this.currentUser = currentUser;
+
+            return {
+              status: true,
+              message: 'user was created',
+            };
+          } else {
+            return {
+              status: false,
+              message: 'username has exist',
+            };
+          }
         },
-        logIn() {},
+        logIn(_username, _password) {
+          if (this.users.length <= 0) {
+            return false;
+          }
+          for (var i = 0; i < this.users.length; i++) {
+            if (
+              this.users[i].username === _username &&
+              this.users[i].password === _password
+            ) {
+              this.currentUser = this.users[i];
+              i = this.users.length;
+              return true;
+            }
+          }
+          return false;
+        },
         isLogin() {
           return !!this.currentUser;
         },
